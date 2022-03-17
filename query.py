573d2a0e51d539ded797df4bf82dcb02c1be7073
xXkenXD <76743023+xXkenXD@users.noreply.github.com>
2022-03-17 20:29:50 +0800

    CRUD completed

--- query.py
+++ query.py
@@ -1,5 +1,4 @@
 
-from asyncio.windows_events import NULL
 import psycopg2
 
 class db:
@@ -22,6 +21,7 @@ class db:
             CREATE TABLE IF NOT EXISTS users (
                     user_id SERIAL PRIMARY KEY,
                     user_name VARCHAR(255) NOT NULL,
+                    password VARCHAR(100) NOT NULL,
                     email VARCHAR(255),
                     role VARCHAR(20)
             )
@@ -57,6 +57,7 @@ class db:
             if conn is not None:
                 conn.close()
 
+    #product table CRUD
     def insert_proddata(prodname,quantity):
         conn = None
         conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
@@ -97,7 +98,7 @@ class db:
         conn = None
         conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
         commands="""SELECT * FROM products"""
-        records=NULL
+        records=None
         try:
             cur = conn.cursor()
             #for command in commands:
@@ -112,6 +113,188 @@ class db:
             if conn is not None:
                 conn.close()
         return records
+
+    def delete_proddata(prodname):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""DELETE from products WHERE prod_name = %s"""
+        value = (prodname)
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands,value)
+            cur.close()
+            conn.commit()
+            print("Product deleted")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    #user table CRUD
+    def insert_user(name,password,email,role):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""INSERT INTO users (user_name, password, email, role) VALUES (%s, %s, %s, %s)"""
+        insert_value=(name,password,email,role)
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands,insert_value)
+            cur.close()
+            conn.commit()
+            print("User Added")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    def update_user(name,password=None,email=None):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        
+        try:
+            cur = conn.cursor()
+            if(password!=None):
+                commands="""UPDATE users SET password = %s WHERE user_name = %s"""
+                value = (password,name)
+                cur.execute(commands,value)
+            #for command in commands:
+            if(email!=None):
+                commands="""UPDATE users SET email = %s WHERE user_name = %s"""
+                value = (email,name)
+                cur.execute(commands,value)
+            cur.close()
+            conn.commit()
+            print("User Updated")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    def read_user():
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""SELECT * FROM users"""
+        records=None
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands)
+            
+            records = cur.fetchall()
+            cur.close()
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+        return records
+
+    def delete_user(name):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""DELETE from users WHERE user_name = %s"""
+        value = (name)
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands,value)
+            cur.close()
+            conn.commit()
+            print("Product deleted")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    #business partner table CRUD
+    def insert_bpdata(name,email,company):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""INSERT INTO business_partner (bp_name, bp_email, company) VALUES (%s, %s, %s)"""
+        insert_value=(name,email,company)
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands,insert_value)
+            cur.close()
+            conn.commit()
+            print("Row Added")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    def update_bp(name,email=None,company=None):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        
+        try:
+            cur = conn.cursor()
+            if(email!=None):
+                commands="""UPDATE business_partner SET bp_email = %s WHERE bp_name = %s"""
+                value = (email,name)
+                cur.execute(commands,value)
+            #for command in commands:
+            if(company!=None):
+                commands="""UPDATE business_partner SET company = %s WHERE bp_name = %s"""
+                value = (company,name)
+                cur.execute(commands,value)
+            #for command in commands:
+            cur.execute(commands,value)
+            cur.close()
+            conn.commit()
+            print("Row Updated")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+
+    def read_proddata():
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""SELECT * FROM business_partner"""
+        records=None
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands)
+            
+            records = cur.fetchall()
+            cur.close()
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+        return records
+
+    def delete_proddata(bp_name):
+        conn = None
+        conn = psycopg2.connect(database="H2H", user = "postgres", password="admin123", host="localhost")
+        commands="""DELETE from business_partner WHERE bp_name = %s"""
+        value = (bp_name)
+        try:
+            cur = conn.cursor()
+            #for command in commands:
+            cur.execute(commands,value)
+            cur.close()
+            conn.commit()
+            print("Row deleted")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
 
 
 
